c767a414f7d0aa53233b7fc2de74b5c7a274c2f4
aliernfrog <45766489+aliernfrog@users.noreply.github.com>
2023-04-09 15:22:22 +0300

    feat(AutomaticTheme): final changes

--- plugins/AutomaticAmoled/src/index.ts
+++ plugins/AutomaticAmoled/src/index.ts
@@ -14,12 +14,14 @@ export default {
         logger.log(`Current theme: ${theme}`);
         logger.log(`AMOLED value: ${amoled}, enabled: ${amoledEnabled}`);
         
-        if (theme != "dark" && amoledEnabled) {
-          logger.log("Disabling AMOLED since dark theme is not enabled");
-          AMOLEDThemeManager.setAMOLEDThemeEnabled(false);
-        } else if (!amoledEnabled) {
+        if (theme == "dark" && !amoledEnabled) {
           logger.log("Enabling AMOLED");
           AMOLEDThemeManager.setAMOLEDThemeEnabled(true);
+        } else if (amoledEnabled) {
+          // Having amoled + light theme causes theming issues on 174.6 (and probably up)
+          // Need to disable it when dark theme is not enabled
+          logger.log("Disabling AMOLED since dark theme is not enabled");
+          AMOLEDThemeManager.setAMOLEDThemeEnabled(false);
         } else {
           logger.log("No need to change preferences");
         }
