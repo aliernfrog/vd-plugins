422170b584471abf32853963dac12b87a75bcc4d
aliernfrog <45766489+aliernfrog@users.noreply.github.com>
2023-04-09 12:13:27 +0300

    fix(AutomaticTheme): import logger properly

--- plugins/AutomaticAmoled/src/index.ts
+++ plugins/AutomaticAmoled/src/index.ts
@@ -1,16 +1,16 @@
 import { findByProps, findByStoreName } from "@vendetta/metro";
-import logger from "@lib/logger";
+import { logger } from "@vendetta";
 
 const ThemeManager = findByProps("updateTheme", "overrideTheme");
 const ThemeStore = findByStoreName("ThemeStore");
 
 export default {
     onLoad: () => {
-        try {
-          logger.log(`ThemeStore.theme = ${ThemeStore.theme}`);
-          logger.log(`${ThemeManager}`);
-        } catch (e) {
-          logger.error("Failed to load AutomaticAmoled plugin.", e);
-        }
+      try {
+        logger.log(`ThemeStore.theme = ${ThemeStore.theme}`);
+        logger.log(`${ThemeManager}`);
+      } catch(e) {
+        logger.error("Failed to load AutomaticAmoled.", e);
+      }
     }
 }
