745ffd87be17e059c36025f94e33c1dc084b6d6c
aliernfrog <45766489+aliernfrog@users.noreply.github.com>
2023-04-09 15:56:29 +0300

    fix(AutomaticAmoled): fix building

--- plugins/AutomaticAmoled/src/index.ts
+++ plugins/AutomaticAmoled/src/index.ts
@@ -6,27 +6,28 @@ const ThemeStore = findByStoreName("ThemeStore");
 const UnsyncedUserSettingsStore = findByStoreName("UnsyncedUserSettingsStore");
 
 export default {
-      try {
-        const theme = ThemeStore.theme || "dark";
-        const amoled = UnsyncedUserSettingsStore.useAMOLEDTheme;
-        const amoledEnabled = amoled == 2;
+  onLoad: () => {
+    try {
+      const theme = ThemeStore.theme || "dark";
+      const amoled = UnsyncedUserSettingsStore.useAMOLEDTheme;
+      const amoledEnabled = amoled == 2;
         
-        logger.log(`Current theme: ${theme}`);
-        logger.log(`AMOLED value: ${amoled}, enabled: ${amoledEnabled}`);
+      logger.log(`Current theme: ${theme}`);
+      logger.log(`AMOLED value: ${amoled}, enabled: ${amoledEnabled}`);
         
-        if (theme == "dark" && !amoledEnabled) {
-          logger.log("Enabling AMOLED");
-          AMOLEDThemeManager.setAMOLEDThemeEnabled(true);
-        } else if (amoledEnabled) {
-          // Having amoled + light theme causes theming issues on 174.6 (and probably up)
-          // Need to disable it when dark theme is not enabled
-          logger.log("Disabling AMOLED since dark theme is not enabled");
-          AMOLEDThemeManager.setAMOLEDThemeEnabled(false);
-        } else {
-          logger.log("No need to change preferences");
-        }
-      } catch(e) {
-        logger.error("Failed to load AutomaticAmoled", e);
+      if (theme == "dark" && !amoledEnabled) {
+        logger.log("Enabling AMOLED");
+        AMOLEDThemeManager.setAMOLEDThemeEnabled(true);
+      } else if (amoledEnabled) {
+        // Having amoled + light theme causes theming issues on 174.6 (and probably up)
+        // Need to disable it when dark theme is not enabled
+        logger.log("Disabling AMOLED since dark theme is not enabled");
+        AMOLEDThemeManager.setAMOLEDThemeEnabled(false);
+      } else {
+        logger.log("No need to change preferences");
       }
+    } catch(e) {
+      logger.error("Failed to load AutomaticAmoled", e);
     }
+  }
 }
