1a2115aff64df01a63f596b750d9f1c77b111ea2
aliernfrog <45766489+aliernfrog@users.noreply.github.com>
2023-04-09 14:53:39 +0300

    feat(AutomaticAmoled): actually implement

--- plugins/AutomaticAmoled/src/index.ts
+++ plugins/AutomaticAmoled/src/index.ts
@@ -1,18 +1,30 @@
 import { findByProps, findByStoreName } from "@vendetta/metro";
 import { logger } from "@vendetta";
 
-const ThemeManager = findByProps("updateTheme", "overrideTheme");
+const AMOLEDThemeManager = findByProps("setAMOLEDThemeEnabled");
 const ThemeStore = findByStoreName("ThemeStore");
 const UnsyncedUserSettingsStore = findByStoreName("UnsyncedUserSettingsStore");
 
 export default {
-    onLoad: () => {
       try {
-        logger.log(`ThemeStore.theme = ${ThemeStore.theme}`);
-        logger.log(`UnsyncedUserSettingsStore = ${UnsyncedUserSettingsStore.theme}`);
-        logger.log(`${JSON.stringify(ThemeManager)}`);
+        const theme = ThemeStore.theme || "dark";
+        const amoled = UnsyncedUserSettingsStore.useAMOLEDTheme;
+        const amoledEnabled = amoled == 2;
+        
+        logger.log(`Current theme: ${theme}`);
+        logger.log(`AMOLED value: ${amoled}, enabled: ${amoledEnabled}`);
+        
+        if (theme != "dark" && amoledEnabled) {
+          logger.log("Disabling AMOLED since dark theme is not enabled");
+          AMOLEDThemeManager.setAMOLEDThemeEnabled(false);
+        } else if (!amoledEnabled) {
+          logger.log("Enabling AMOLED");
+          AMOLEDThemeManager.setAMOLEDThemeEnabled(true);
+        } else {
+          logger.log("No need to change preferences");
+        }
       } catch(e) {
-        logger.error("Failed to load AutomaticAmoled.", e);
+        logger.error("Failed to load AutomaticAmoled", e);
       }
     }
 }
