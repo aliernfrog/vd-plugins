6da952fa18a19c96a41f1d945246a0258c3c40dc
aliernfrog <45766489+aliernfrog@users.noreply.github.com>
2023-04-09 13:35:25 +0300

    feat(AutomaticAmoled): log user pref

--- plugins/AutomaticAmoled/src/index.ts
+++ plugins/AutomaticAmoled/src/index.ts
@@ -3,11 +3,13 @@ import { logger } from "@vendetta";
 
 const ThemeManager = findByProps("updateTheme", "overrideTheme");
 const ThemeStore = findByStoreName("ThemeStore");
+const UnsyncedUserSettingsStore = findByStoreName("UnsyncedUserSettingsStore");
 
 export default {
     onLoad: () => {
       try {
         logger.log(`ThemeStore.theme = ${ThemeStore.theme}`);
+        logger.log(`UnsyncedUserSettingsStore = ${UnsyncedUserSettingsStore.theme}`);
         logger.log(`${JSON.stringify(ThemeManager)}`);
       } catch(e) {
         logger.error("Failed to load AutomaticAmoled.", e);
