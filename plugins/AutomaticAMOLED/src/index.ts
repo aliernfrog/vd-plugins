52aae32f04de7330ccc13096b7ba7ef4ef2638b5
aliernfrog <45766489+aliernfrog@users.noreply.github.com>
2023-04-20 15:08:23 +0300

    feat(AutomaticAMOLED): settings

--- plugins/AutomaticAMOLED/src/index.ts
+++ plugins/AutomaticAMOLED/src/index.ts
@@ -1,5 +1,8 @@
 import { findByProps, findByStoreName } from "@vendetta/metro";
+import { storage } from "@vendetta/plugin";
 import { logger } from "@vendetta";
+
+import Settings from "./ui/Settings";
 
 const AMOLEDThemeManager = findByProps("setAMOLEDThemeEnabled");
 const ThemeStore = findByStoreName("ThemeStore");
@@ -8,18 +11,22 @@ const UnsyncedUserSettingsStore = findByStoreName("UnsyncedUserSettingsStore");
 export default {
   onLoad: () => {
     try {
+      storage.disableWhenLight ??= true;
+      
       const theme = ThemeStore.theme || "dark";
       const amoled = UnsyncedUserSettingsStore.useAMOLEDTheme;
       const darkEnabled = theme == "dark";
       const amoledEnabled = amoled == 2;
+      const disableWhenLight = storage.disableWhenLight;
         
       logger.log(`Current theme: ${theme}, dark mode: ${darkEnabled}`);
       logger.log(`AMOLED value: ${amoled}, enabled: ${amoledEnabled}`);
-        
+      logger.log(`Disable when light: ${disableWhenLight}`);
+      
       if (darkEnabled && !amoledEnabled) {
         logger.log("Enabling AMOLED");
         AMOLEDThemeManager.setAMOLEDThemeEnabled(true);
-      } else if (!darkEnabled && amoledEnabled) {
+      } else if (!darkEnabled && amoledEnabled && disableWhenLight) {
         // Having AMOLED + light theme causes theming issues on 174.6 (and probably wont be fixed)
         // Need to disable it when dark theme is not enabled
         logger.log("Disabling AMOLED since dark theme is not enabled");
@@ -30,5 +37,6 @@ export default {
     } catch(e) {
       logger.error("Failed to load AutomaticAmoled", e);
     }
-  }
+  },
+  settings: Settings
 }
---
+++ plugins/AutomaticAMOLED/src/ui/Settings.tsx
@@ -0,0 +1,18 @@
+import { storage } from "@vendetta/plugin";
+import { Forms, General } from "@vendetta/ui/components";
+
+const { FormSwitchRow } = Forms;
+const { ScrollView } = General;
+
+export default function Settings() {
+  return <ScrollView>
+    <FormSwitchRow
+      label="Disable AMOLED theme when light theme is enabled"
+      subLabel="AMOLED + light theme causes issues on 174.6 and above."
+      value={storage.disableWhenLight}
+      onValueChange={(v: boolean) => {
+        storage.disableWhenLight = v;
+      }}
+    />
+  </ScrollView>
+}
