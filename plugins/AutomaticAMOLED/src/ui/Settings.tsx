01a989b99095dfc1a3e6f4172fe0a2e30d3088c7
aliernfrog <45766489+aliernfrog@users.noreply.github.com>
2023-04-20 15:19:34 +0300

    fix(AutomaticAMOLED): proxy

--- plugins/AutomaticAMOLED/src/ui/Settings.tsx
+++ plugins/AutomaticAMOLED/src/ui/Settings.tsx
@@ -1,10 +1,12 @@
 import { storage } from "@vendetta/plugin";
 import { Forms, General } from "@vendetta/ui/components";
+import { useProxy } from '@vendetta/storage';
 
 const { FormSwitchRow } = Forms;
 const { ScrollView } = General;
 
 export default function Settings() {
+  useProxy(storage);
   return <ScrollView>
     <FormSwitchRow
       label="Disable AMOLED theme when light theme is enabled"
