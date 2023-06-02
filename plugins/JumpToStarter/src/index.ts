5a9c269a7f3089c46f671cc49eb2a3b1bb84c624
aliernfrog <45766489+aliernfrog@users.noreply.github.com>
2023-06-02 16:15:42 +0300

    cleanup

--- plugins/JumpToStarter/src/index.ts
+++ plugins/JumpToStarter/src/index.ts
@@ -1,26 +1,15 @@
-import { logger } from "@vendetta";
-import { findByName, findByProps } from "@vendetta/metro";
+import { findByName } from "@vendetta/metro";
 import { after } from "@vendetta/patcher";
 import JumpButton from "./ui/JumpButton";
 
 const ForumPostLongPressActionSheet = findByName("ForumPostLongPressActionSheet", false);
-//const { useFirstForumPostMessage } = findByProps("useFirstForumPostMessage");
 
 let patch;
 
 export default {
   onLoad: () => {
     patch = after("default", ForumPostLongPressActionSheet, ([{ thread }], res) => {
-      //const { firstMessage } = useFirstForumPostMessage(thread);
-      
-      //if (!firstMessage) return logger.log(`Forum thread ${thread.id} doesn't have a starter message`);
-      
-      //logger.log(`First message: ${JSON.stringify(firstMessage)}`);
-      logger.log(`Thread: ${JSON.stringify(thread)}`);
-      
       const firstMessageURL = `https://discord.com/channels/${thread.guild_id}/${thread.id}/${thread.id}`;
-      logger.log(`First message URL: ${firstMessageURL}`);
-      
       JumpButton(res, firstMessageURL);
     });
   },
--- plugins/JumpToStarter/src/ui/JumpButton.tsx
+++ plugins/JumpToStarter/src/ui/JumpButton.tsx
@@ -1,4 +1,4 @@
-import { findByName, findByProps } from "@vendetta/metro";
+import { findByName } from "@vendetta/metro";
 import { url } from "@vendetta/metro/common";
 import { getAssetIDByName } from "@vendetta/ui/assets";
 import { findInReactTree } from "@vendetta/utils";
@@ -7,8 +7,6 @@ import { Forms } from "@vendetta/ui/components";
 const { FormRow } = Forms;
 const Icon = findByName("Icon");
 
-//const { hideActionSheet } = findByProps("openLazy", "hideActionSheet");
-
 export default function JumpButton(res, firstMessageURL) {
   const actions = findInReactTree(res, (t) => t.props?.bottom === true).props.children.props.children[1];
   const ActionsSection = actions[0].type;
@@ -16,9 +14,8 @@ export default function JumpButton(res, firstMessageURL) {
   actions.unshift(<ActionsSection key="jumpfirst">
     <FormRow
       leading={<Icon source={getAssetIDByName("ic_link_24px")} />}
-      label={"Jump to first message"}
+      label={"Jump to starter message"}
       onPress={() =>
-        //(hideActionSheet())
         (url.openDeeplink(firstMessageURL))
       }
     />
