f2914b87470b7656590ff9646881c9b169189144
aliernfrog <45766489+aliernfrog@users.noreply.github.com>
2023-06-02 11:55:01 +0300

    feat(jump-to-starter): init

---
+++ plugins/JumpToStarter/src/index.ts
@@ -0,0 +1,24 @@
+import { findByName, findByProps } from "@vendetta/metro";
+import { logger } from "@vendetta";
+import { after } from "@vendetta/patcher";
+
+const ForumPostLongPressActionSheet = findByName("ForumPostLongPressActionSheet", false);
+const { useFirstForumPostMessage } = findByProps("useFirstForumPostMessage");
+
+let patch;
+
+export default {
+  onLoad: () => {
+    patch = after("default", ForumPostLongPressActionSheet, ([{ thread }], res) => {
+      const { firstMessage } = useFirstForumPostMessage(thread);
+      
+      if (!firstMessage) return logger.log(`Forum thread ${thread.id} doesn't have a starter message`);
+      
+      logger.log(`First message: ${JSON.stringify(firstMessage)}`);
+    });
+  },
+  
+  onUnload: () => {
+    patch?.unpatch();
+  }
+}
