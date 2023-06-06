b79bb31e30d32c08a4f970284232f96d5a45177a
aliernfrog <45766489+aliernfrog@users.noreply.github.com>
2023-06-06 22:45:19 +0300

    feat: check if sticker is from current guild

--- plugins/FreeStickers/src/index.ts
+++ plugins/FreeStickers/src/index.ts
@@ -4,6 +4,7 @@ import { instead } from "@vendetta/patcher";
 
 const nitroInfo = findByProps("canUseStickersEverywhere");
 const messageModule = findByProps("sendMessage", "receiveMessage");
+const { getChannel } = findByStoreName("ChannelStore");
 const { getStickerById } = findByStoreName("StickersStore");
 
 const patches = [];
@@ -21,7 +22,7 @@ export default {
       const channelId = args[0];
       const stickerIds = args[1];
       const stickers = stickerIds.map(stickerId => getStickerById(stickerId));
-      const stickersToModify = stickers.filter(sticker => !isStickerAvailable(sticker));
+      const stickersToModify = stickers.filter(sticker => !isStickerAvailable(sticker, channelId));
       if (!stickersToModify.length) return sendStickersOriginal(...args);
       
       const newContent = stickersToModify.map(sticker => buildStickerURL(sticker)).join("\n");
@@ -39,12 +40,14 @@ export default {
   }
 }
 
-function isStickerAvailable(sticker) {
+function isStickerAvailable(sticker, channelId) {
   const isCustom = (sticker.format_type == 1 || // PNG
     sticker.format_type == 2 || // APNG
     sticker.format_type == 4 // GIF
   );
   if (!isCustom) return true;
+  const channelGuildId = getChannel(channelId).guild_id;
+  if (sticker.guild_id && sticker.guild_id == channelGuildId) return true;
   return false;
 }
 
