1ad27fb150b0346dc0d93b49f41a6981704853ce
aliernfrog <45766489+aliernfrog@users.noreply.github.com>
2023-06-13 07:48:57 +0000

    feat(free-stickers): confirmation dialog for apng stickers

--- plugins/FreeStickers/src/index.ts
+++ plugins/FreeStickers/src/index.ts
@@ -1,5 +1,7 @@
 import { findByProps, findByStoreName } from "@vendetta/metro";
 import { instead } from "@vendetta/patcher";
+import { storage } from "@vendetta/plugin";
+import { showConfirmationAlert } from "@vendetta/ui/alerts";
 
 const nitroInfo = findByProps("canUseStickersEverywhere");
 const messageModule = findByProps("sendMessage", "receiveMessage");
@@ -21,14 +23,30 @@ export default {
       const stickers = stickerIds.map(stickerId => getStickerById(stickerId));
       const stickersToModify = stickers.filter(sticker => !isStickerAvailable(sticker, channelId));
       if (!stickersToModify.length) return sendStickersOriginal(...args);
-      
-      const newContent = stickersToModify.map(sticker => buildStickerURL(sticker)).join("\n");
-      messageModule.sendMessage(
-        channelId,
-        {
-          content: newContent
-        }
-      );
+
+      const sendStickers = (confirmedDialog) => {
+        if (confirmedDialog) storage.acknowledgedApng = true;
+        const newContent = stickersToModify.map(sticker => buildStickerURL(sticker)).join("\n");
+        messageModule.sendMessage(
+          channelId,
+          {
+            content: newContent
+          }
+        );
+      }
+
+      const showApngConfirmation = (!!stickersToModify.find(sticker => sticker.format_type == 2)) && !storage.acknowledgedApng;
+      if (showApngConfirmation) {
+        showConfirmationAlert({
+          title: "APNG stickers",
+          content: "APNG stickers are not supported by FreeStickers and will be non-animated in chat. Do you want to send it anyway?",
+          confirmText: "Send anyway",
+          cancelText: "Cancel",
+          onConfirm: () => {
+            sendStickers(true)
+          }
+        })
+      } else sendStickers();
     }));
   },
   
