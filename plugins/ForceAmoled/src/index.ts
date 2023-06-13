9cc54f0cceb43a57afab592dc00b4260b0f1aa8a
aliernfrog <45766489+aliernfrog@users.noreply.github.com>
2023-06-13 19:12:35 +0000

    chore: cleanup

--- plugins/ForceAmoled/src/index.ts
+++
@@ -1,9 +0,0 @@
-import { findByProps } from "@vendetta/metro";
-
-const AMOLEDThemeManager = findByProps("setAMOLEDThemeEnabled");
-
-export default {
-    onLoad: () => {
-        AMOLEDThemeManager.setAMOLEDThemeEnabled(true);
-    }
-}
