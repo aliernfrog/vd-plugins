e69c4bc83c1bb7587de1a8039cf6b7e7b4139973
aliernfrog <45766489+aliernfrog@users.noreply.github.com>
2023-06-29 10:00:35 +0000

    feat(plugin): `HideVoiceButton` plugin

---
+++ plugins/HideVoiceButton/src/index.ts
@@ -0,0 +1,15 @@
+import { logger } from "@vendetta";
+import { findByName } from "@vendetta/metro";
+import { after } from "@vendetta/patcher";
+
+const ChatInput = findByName("ChatInput");
+
+const patch = after("render", ChatInput.prototype, (...args) => {
+  try {
+    args[1].props.children[2].props.children.props.children[1].props.canSendVoiceMessage = false;
+  } catch (e) {
+    logger.error("Failed to hide voice message button", e);
+  }
+});
+
+export const onUnload = () => patch();
