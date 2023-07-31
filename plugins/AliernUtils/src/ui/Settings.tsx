49c8a86457cfb68c0bfaafb33abcc919dad8cfa4
aliernfrog <45766489+aliernfrog@users.noreply.github.com>
2023-07-31 20:13:21 +0000

    feat(Plugins > AliernUtils): Some adjustments to settings

--- plugins/AliernUtils/src/ui/Settings.tsx
+++ plugins/AliernUtils/src/ui/Settings.tsx
@@ -5,6 +5,7 @@ import { Forms, General } from "@vendetta/ui/components";
 const { FormSection, FormSwitchRow } = Forms;
 const { ScrollView } = General;
 
+storage.AlwaysTabsV2 ??= true;
 storage.ChatAnimations ??= true;
 
 export default function Settings(reloadPatches) {
@@ -14,20 +15,20 @@ export default function Settings(reloadPatches) {
     return <ScrollView>
       <FormSection title="Useful experiments" titleStyleType="no_border">
         <FormSwitchRow
-          label="Chat animations (Android)"
-          subLabel="Enable chat animations for Android"
-          value={storage.ChatAnimations}
-          onValueChange={(v) => {
-            storage.ChatAnimations = v;
-            reloadPatches();
-          }}
-        />
-        <FormSwitchRow
           label="Tabs v2"
           subLabel="Enable Tabs v2"
           value={storage.AlwaysTabsV2}
           onValueChange={(v) => {
             storage.AlwaysTabsV2 = v;
+            reloadPatches();
+          }}
+        />
+        <FormSwitchRow
+          label="Chat animations (Android)"
+          subLabel="Enable chat animations for Android"
+          value={storage.ChatAnimations}
+          onValueChange={(v) => {
+            storage.ChatAnimations = v;
             reloadPatches();
           }}
         />
