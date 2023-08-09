bd72ae005794e5b3aa6c214a8644a798ff385344
aliernfrog <45766489+aliernfrog@users.noreply.github.com>
2023-08-09 08:35:32 +0000

    feat(plugins/aliern-utils): add icons to settings

--- plugins/AliernUtils/src/ui/Settings.tsx
+++ plugins/AliernUtils/src/ui/Settings.tsx
@@ -1,8 +1,9 @@
 import { storage } from "@vendetta/plugin";
 import { useProxy } from "@vendetta/storage";
+import { getAssetIDByName } from "@vendetta/ui/assets";
 import { Forms, General } from "@vendetta/ui/components";
 
-const { FormSection, FormSwitchRow } = Forms;
+const { FormIcon, FormSection, FormSwitchRow } = Forms;
 const { ScrollView } = General;
 
 storage.AlwaysTabsV2 ??= true;
@@ -17,6 +18,7 @@ export default function Settings(reloadPatches) {
         <FormSwitchRow
           label="Tabs v2"
           subLabel="Enable Tabs v2"
+          leading={getAssetIDByName("ic_mobile_device")}
           value={storage.AlwaysTabsV2}
           onValueChange={(v) => {
             storage.AlwaysTabsV2 = v;
@@ -26,6 +28,7 @@ export default function Settings(reloadPatches) {
         <FormSwitchRow
           label="Chat animations (Android)"
           subLabel="Enable chat animations for Android"
+          leading={getAssetIDByName("ic_chat")}
           value={storage.ChatAnimations}
           onValueChange={(v) => {
             storage.ChatAnimations = v;
