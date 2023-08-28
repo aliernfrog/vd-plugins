a2c73f6414f2dfe145edf88b89c60412338a07d9
aliernfrog <45766489+aliernfrog@users.noreply.github.com>
2023-08-28 18:26:06 +0000

    fix(aliern-utils/chat-animations): build error

--- plugins/AliernUtils/src/patches/ChatAnimations.ts
+++ plugins/AliernUtils/src/patches/ChatAnimations.ts
@@ -4,7 +4,7 @@ import { storage } from "@vendetta/plugin";
 const { ChatListAnimationExperiment } = findByProps("ChatListAnimationExperiment");
 
 export default function () {
-  // TODO this crashes on 194.7
+  // This partially works and crashes on 194.7+
   try {
     const config = ChatListAnimationExperiment.getCurrentConfig();
   
@@ -13,7 +13,7 @@ export default function () {
     return () => {
       config.shouldAnimateAndroid = false;
     }
-  } catch () {
+  } catch (_) {
     return () => {}
   }
 }
