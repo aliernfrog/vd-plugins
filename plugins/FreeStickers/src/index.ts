2ae62809e82f948fe29ec307ad7b532d8a8e2114
aliernfrog <45766489+aliernfrog@users.noreply.github.com>
2023-09-08 17:10:58 +0000

    fix(plugins/free-stickers): fix for 194.4+

--- plugins/FreeStickers/src/index.ts
+++ plugins/FreeStickers/src/index.ts
@@ -1,11 +1,16 @@
-import { findByProps, findByStoreName } from "@vendetta/metro";
+import { find, findByProps, findByStoreName } from "@vendetta/metro";
 import { instead } from "@vendetta/patcher";
 import { storage } from "@vendetta/plugin";
 import { showConfirmationAlert } from "@vendetta/ui/alerts";
 import { defaultStickerURL, buildStickerURL, isStickerAvailable } from "./utils";
 import Settings from "./ui/Settings";
 
-const nitroInfo = findByProps("canUseStickersEverywhere");
+// On 194.4+, this nitro module is frozen
+// original fix from enmity plugin: https://github.com/colin273/enmity-plugins/blob/master/Freemoji/src/index.ts#L59
+// vendetta version: https://discord.com/channels/1015931589865246730/1062531774187573308/1149552971235995698
+const nitroModule = find(m => m.default?.canUseStickersEverywhere);
+nitroModule.default = { ... nitroModule.default }
+
 const messageModule = findByProps("sendMessage", "receiveMessage");
 const { getStickerById } = findByStoreName("StickersStore");
 
@@ -16,16 +21,15 @@ export default {
     storage.stickerURL ??= defaultStickerURL;
     
     patches.push(
-      instead("canUseStickersEverywhere", nitroInfo, () => true)
+      instead("canUseStickersEverywhere", nitroModule.default, () => true)
     );
     
-    const sendStickersOriginal = messageModule.sendStickers;
-    patches.push(instead("sendStickers", messageModule, (args) => {
+    patches.push(instead("sendStickers", messageModule, (args, orig) => {
       const [ channelId, stickerIds, _, extra ] = args;
       
       const stickers = stickerIds.map(stickerId => getStickerById(stickerId));
       const stickersToModify = stickers.filter(sticker => !isStickerAvailable(sticker, channelId));
-      if (!stickersToModify.length) return sendStickersOriginal(...args);
+      if (!stickersToModify.length) return orig(...args);
 
       const sendStickers = (confirmedDialog) => {
         if (confirmedDialog) storage.acknowledgedApng = true;
