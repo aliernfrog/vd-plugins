e0235a300ca31b12618077f4fc5445a03251467b
alieRN <45766489+aliernfrog@users.noreply.github.com>
2024-04-05 20:55:28 +0300

    fix(free-stickers): remove unfreeze logic

--- plugins/FreeStickers/src/index.ts
+++ plugins/FreeStickers/src/index.ts
@@ -5,11 +5,12 @@ import { showConfirmationAlert } from "@vendetta/ui/alerts";
 import { buildStickerURL, isStickerAvailable } from "./utils";
 import Settings from "./ui/Settings";
 
+
 // On 194.4+, this nitro module is frozen
 // original fix from enmity plugin: https://github.com/colin273/enmity-plugins/blob/master/Freemoji/src/index.ts#L59
 // vendetta version: https://discord.com/channels/1015931589865246730/1062531774187573308/1149552971235995698
 const nitroModule = find(m => m.default?.canUseAnimatedEmojis);
-nitroModule.default = { ... nitroModule.default }
+//nitroModule.default = { ... nitroModule.default }
 
 const messageModule = findByProps("sendMessage", "receiveMessage");
 const { getStickerById } = findByStoreName("StickersStore");
