6c09362255ab093f3c07846ec9babe2b95bdb0d7
alieRN <45766489+aliernfrog@users.noreply.github.com>
2024-04-22 12:07:07 +0300

    feat(jump-to/forum-sheet): drop old design

--- plugins/JumpTo/src/patches/ForumPostActionSheet.tsx
+++ plugins/JumpTo/src/patches/ForumPostActionSheet.tsx
@@ -2,20 +2,17 @@ import { findByName, findByProps } from "@vendetta/metro";
 import { clipboard, url } from "@vendetta/metro/common";
 import { after } from "@vendetta/patcher";
 import { getAssetIDByName } from "@vendetta/ui/assets";
-/* Only required for old patch -> */ import { Forms } from "@vendetta/ui/components";
 import { showToast } from "@vendetta/ui/toasts";
 import { findInReactTree } from "@vendetta/utils";
 import { buildStarterURL } from "../utils";
 
 const { hideActionSheet } = findByProps("openLazy", "hideActionSheet");
-const ActionSheetRow = findByProps("ActionSheetRow")?.ActionSheetRow;
+const { ActionSheetRow } = findByProps("ActionSheetRow");
 const ForumPostLongPressActionSheet = findByName("ForumPostLongPressActionSheet", false);
-const CopyIcon = getAssetIDByName("ic_copy_message_link");
-const LinkIcon = getAssetIDByName("toast_copy_link");
+const CopyLinkIcon = getAssetIDByName("ic_copy_message_link");
+const ToastLinkIcon = getAssetIDByName("toast_copy_link");
 
-/* Only required for old patch -> */ const { FormIcon, FormRow } = Forms;
-
-const newPatch = () => after("default", ForumPostLongPressActionSheet, ([{thread}], component) => {
+export default after("default", ForumPostLongPressActionSheet, ([{thread}], component) => {
   const actions = findInReactTree(component, (c) =>
     c?.[0]?.type?.name === "ActionSheetRowGroup"
   );
@@ -30,7 +27,7 @@ const newPatch = () => after("default", ForumPostLongPressActionSheet, ([{thread
         label="Jump To Starter Message"
         icon={
           <ActionSheetRow.Icon
-            source={CopyIcon}
+            source={CopyLinkIcon}
           />
         }
         onPress={() => {
@@ -38,42 +35,10 @@ const newPatch = () => after("default", ForumPostLongPressActionSheet, ([{thread
         }}
         onLongPress={() => {
           clipboard.setString(starterURL);
-          showToast("Copied starter message URL", LinkIcon);
+          showToast("Copied starter message URL", ToastLinkIcon);
           hideActionSheet();
         }}
       />
     </ActionSheetRow.Group>
   );
 });
-
-const oldPatch = () => after("default", ForumPostLongPressActionSheet, ([{thread}], component) => {
-  const actions = findInReactTree(component, (c) => c?.props?.bottom)?.props?.children?.props?.children?.[1];
-  if (!actions) return;
-
-  const starterURL = buildStarterURL(thread);
-  const ActionsSection = actions[0].type;
-  
-  actions.unshift(
-    <ActionsSection key="jumptovd">
-      <FormRow
-        label="Jump To Starter Message"
-        leading={
-          <FormIcon
-            source={CopyIcon}
-            style={{ opacity: 1 }}
-          />
-        }
-        onPress={() => {
-          url.openDeeplink(starterURL);
-        }}
-        onLongPress={() => {
-          clipboard.setString(starterURL);
-          showToast("Copied starter message URL", LinkIcon);
-          hideActionSheet();
-        }}
-      />
-    </ActionsSection>
-  );
-});
-
-export default ActionSheetRow ? newPatch() : oldPatch();
