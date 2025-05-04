15ef08e19d7a56ac9974ab4eb329bc4a5efd0846
aliernfrog <45766489+aliernfrog@users.noreply.github.com>
2025-05-04 11:22:44 +0000

    feat: fork ViewRaw and match code style

---
+++ plugins/ViewRaw/src/cleanMessage.ts
@@ -0,0 +1,18 @@
+const sortObject = <T extends object>(obj: T): T  => {
+  return Object.fromEntries(Object.entries(obj).sort(([k1], [k2]) => k1.localeCompare(k2))) as T;
+}
+
+export const cleanMessage = (msg) => {
+  const clone = JSON.parse(JSON.stringify(msg));
+  for (const key in clone.author) {
+    switch (key) {
+      case "email":
+      case "phone":
+      case "mfaEnabled":
+      case "hasBouncedEmail":
+        delete clone.author[key];
+    }
+  }
+
+  return clone
+}
---
+++ plugins/ViewRaw/src/index.tsx
@@ -0,0 +1,54 @@
+import { before, after } from "@vendetta/patcher";
+import { getAssetIDByName } from "@vendetta/ui/assets";
+import { findInReactTree } from "@vendetta/utils";
+import { findByName, findByProps } from "@vendetta/metro";
+import { React } from "@vendetta/metro/common";
+import { Forms } from "@vendetta/ui/components";
+import RawPage from "./RawPage";
+
+const ActionSheet = findByProps("openLazy", "hideActionSheet");
+const Navigation = findByProps("push", "pushLazy", "pop");
+const modalCloseButton =
+  findByProps("getRenderCloseButton")?.getRenderCloseButton ??
+  findByProps("getHeaderCloseButton")?.getHeaderCloseButton;
+const Navigator = findByName("Navigator") ?? findByProps("Navigator")?.Navigator;
+const { FormRow, FormIcon } = Forms;
+
+const unpatch = before("openLazy", ActionSheet, ([component, key, msg]) => {
+  const message = msg?.message;
+  if (key !== "MessageLongPressActionSheet" || !message) return;
+  component.then(instance => {
+    const unpatch = after("default", instance, (_, component) => {
+      React.useEffect(() => () => { unpatch() }, []);
+      const buttons = findInReactTree(component, x => x?.[0]?.type?.name === "ButtonRow");
+      if (!buttons) return;
+
+      const navigator = () => (
+        <Navigator
+          initialRouteName="RawPage"
+          goBackOnBackPress
+          screens={{
+            RawPage: {
+              title: "ViewRaw",
+              headerLeft: modalCloseButton?.(() => Navigation.pop()),
+              render: () => <RawPage message={message} />
+            }
+          }}
+        />
+      );
+
+      buttons.push(
+        <FormRow
+          label="View Raw"
+          leading={<FormIcon style={{ opacity: 1 }} source={getAssetIDByName("ic_chat_bubble_16px")} />}
+          onPress={() => {
+            ActionSheet.hideActionSheet();
+            Navigation.push(navigator);
+          }}
+        />
+      );
+    });
+  });
+});
+
+export const onUnload = () => unpatch();
